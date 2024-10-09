a0856551adf5af550c709534755e94bb90d1b7d1
meganasampson <megan.anne.sampson@gmail.com>
2024-10-09 10:53:22 +0100

    generator generates csv instead of images

--- data_sim.py
+++ data_sim.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import csv
+import pandas as pd
 
 
 
@@ -126,3 +127,176 @@ class CameraDataGenerator:
 
 
         self._image_count += 1
+
+class ExperimentDataGenerator:
+    def __init__(self, frequency: float = 1.0) -> None:
+
+        self._frequency = frequency
+        self._target_path = Path.cwd()
+
+        self._csv_count = 0
+
+        self._trace_file = None
+        self._csv_files = list([])
+
+        self._trace_file_tag = 'csv'
+        self._csv_file_tag = 'csv'
+
+
+    def set_target_path(self, targ_path: Path) -> None:
+
+        if not targ_path.is_dir():
+            raise FileNotFoundError("Target path does not exist.")
+
+        self._target_path = targ_path
+
+
+    def load_csv_files(self, in_files) -> None:
+
+        #self._csv_files = list([])
+        csv_df = pd.read_csv(in_files)
+        self._csv_files = csv_df
+        #self._csv_files.append(np.array(csv_df))
+
+
+    def reset(self) -> None:
+        self._csv_count = 0
+
+
+    def generate_data(self, duration: float, std_dev: int) -> None:
+
+        if duration < 0.0:
+            raise DataGeneratorError("Data generation duration must be greater than 0")
+
+        duration_timer = Timer(duration)
+        output_timer = Timer(1.0/self._frequency)
+
+        duration_timer.start()
+        output_timer.start()
+
+        while not duration_timer.finished():
+            if output_timer.finished():
+                output_timer.start()
+                print(f'Duration = {duration_timer.elapsed_time()}s')
+
+                self.write_traces()
+                self.write_csv(std_dev)
+
+
+    def write_traces(self) -> None:
+        print('Writing traces')
+
+
+    def write_csv(self, std_dev) -> None:
+
+        for nn,ii in enumerate(self._csv_files):
+            #0 = mean, 10 = standard deviation
+            n_bits = 8
+            #noise = np.random.normal(0, std_dev, size=ii.shape)
+            #noise = np.random.default_rng().standard_normal(ii.shape)
+            #noise_bits = noise*2**n_bits*std_dev/100
+            #img_noised = ii + noise_bits
+            #final_image = np.array(img_noised,dtype=np.uint8)
+            csv_num_str = str(self._csv_count).zfill(4)
+            save_file = f'{self._csv_file_tag}_{csv_num_str }_{nn}.csv'
+            #save_path = self._target_path / save_file
+
+            headers = [i for i in self._csv_files]
+
+            csv_list = []
+            for i in headers:
+                row = self._csv_files[i]
+                csv_list.append(row)
+
+            #im = Image.fromarray(ii)
+            #plt.imsave(save_file, img_noised, cmap="gray")
+            #plt.imsave(save_file, final_image, cmap="gray")
+            #im.save(save_path)
+
+
+            with open(save_file, 'w') as f:
+     
+                # using csv.writer method from CSV package
+                write = csv.writer(f)
+     
+                write.writerow(headers)
+                write.writerows(csv_list)
+
+
+        self._csv_count += 1
+
+class ExperimentMock:
+    def __init__(self, frequency: float = 1.0) -> None:
+
+        self._frequency = frequency
+        self._target_path = Path.cwd()
+
+        self._csv_count = 0
+
+        self._trace_file = None
+        self._csv_files = list([])
+
+        self._trace_file_tag = 'csv'
+        self._csv_file_tag = 'csv'
+    
+    def generate_data(self, duration: float, std_dev: int) -> None:
+
+        if duration < 0.0:
+            raise DataGeneratorError("Data generation duration must be greater than 0")
+
+        duration_timer = Timer(duration)
+        output_timer = Timer(1.0/self._frequency)
+
+        duration_timer.start()
+        output_timer.start()
+
+        while not duration_timer.finished():
+            if output_timer.finished():
+                output_timer.start()
+                print(f'Duration = {duration_timer.elapsed_time()}s')
+
+                self.write_traces()
+                self.write_csv(std_dev)
+
+    def write_traces(self) -> None:
+        print('Writing traces')
+
+    def write_csv(self, std_dev) -> None:
+
+        for nn,ii in enumerate(self._csv_files):
+            #0 = mean, 10 = standard deviation
+            n_bits = 8
+            #noise = np.random.normal(0, std_dev, size=ii.shape)
+            #noise = np.random.default_rng().standard_normal(ii.shape)
+            #noise_bits = noise*2**n_bits*std_dev/100
+            #img_noised = ii + noise_bits
+
+            headers = [i for i in self._csv_files]
+
+            csv_list = []
+            for i in headers:
+                row = self._csv_files[i]
+                csv_list.append(row)
+
+            #im = Image.fromarray(ii)
+            #plt.imsave(save_file, img_noised, cmap="gray")
+            #plt.imsave(save_file, final_image, cmap="gray")
+            #im.save(save_path)
+            
+            #with open("sample.csv", "a") as csvFile:
+                #fieldnames = ['Image path']
+                #writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+
+                #writer.writeheader()
+                #writer.writerow({'Image path': save_path })
+
+            with open('out.csv', 'w') as f:
+     
+                # using csv.writer method from CSV package
+                write = csv.writer(f)
+     
+                write.writerow(headers)
+                write.writerows(csv_list)
+
+
+        self._csv_count += 1
