75ca533ed45a12d178dce84080230fe18ee529c1
meganasampson <megan.anne.sampson@gmail.com>
2024-10-14 14:51:23 +0100

    added argparse arguments

--- csv_gen.py
+++ csv_gen.py
@@ -2,14 +2,20 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-#import pycdata
 from data_sim import *
 import os
 import pandas as pd
 import csv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--duration", default = 3.1, type = float)
+parser.add_argument("--std_dev", default = 10, type = float)
+parser.add_argument("--basefile", default = 'data/newcsvfile.csv', type = str)
+
+args = parser.parse_args()
 
 example = ExperimentDataGenerator()
 #base_file = pd.read_csv('data/newcsvfile.csv')
-base_file = 'data/newcsvfile.csv'
-example.load_csv_files(base_file)
-example.generate_data(duration=3.1, std_dev=10)
+example.load_csv_files(args.basefile)
+example.generate_data(duration = args.duration, std_dev = args.std_dev)
